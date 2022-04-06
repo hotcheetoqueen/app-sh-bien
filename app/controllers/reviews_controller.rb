0c7ab7b8bb0289bcefe63bf5947a894719b70829
hotcheetoqueen <jessicalperelman@gmail.com>
2022-04-06 16:43:56 -0700

    save review form submissions to db

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,16 +5,25 @@ class ReviewsController < ApplicationController
 
         @number = rand(100)
 
-        @reviews = [
-            "The Smile",
-            "Katz Deli",
-            "Baby Bo's",
-            "Peter Pan Donuts"
-        ]
+        @reviews = Review.all
     end
 
     def new
         @review = Review.new
+    end
+
+    def create
+        #grab info from form
+        @review = Review.new(params.require(:review).permit(:title, :body, :score))
+
+        #save to database
+        @review.save
+
+        redirect_to root_path
+    end
+
+    def show
+        @review = Review.find(params[:id])
     end
 
 end

3b1e41f48ee601782982c4053001951cb5fc5746
hotcheetoqueen <jessicalperelman@gmail.com>
2022-04-06 17:14:10 -0700

    allow delete review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,4 +26,11 @@ class ReviewsController < ApplicationController
         @review = Review.find(params[:id])
     end
 
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+
+        redirect_to root_path
+    end
+    
 end
